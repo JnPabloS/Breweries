18f53af040e2eaf922596fc965a443455fcb34dd
JnPabloS <juanps2007@hotmail.com>
2021-09-18 12:58:36 -0500

    Correccion del mapa

--- app/src/main/java/com/jnpablos/breweries/ui/DetailFragment.kt
+++ app/src/main/java/com/jnpablos/breweries/ui/DetailFragment.kt
@@ -6,6 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jnpablos.breweries.R
 import com.jnpablos.breweries.databinding.FragmentDetailBinding
 
@@ -14,25 +19,49 @@ class DetailFragment : Fragment() {
     private val args : DetailFragmentArgs by navArgs()
     private var _binding : FragmentDetailBinding? = null
     private val binding get() = _binding!!
+    private lateinit var breweryName : String
+    private lateinit var lat : String
+    private lateinit var lon : String
+
+    private val callback = OnMapReadyCallback { googleMap ->
+
+        val brwry = LatLng(lat.toDouble(), lon.toDouble())
+        googleMap.addMarker(MarkerOptions().position(brwry).title(breweryName))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(brwry,15F))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
 
         val root: View = binding.root
 
         with(binding) {
+
             val brewery = args.brewery
-            nameTextView.text = brewery.name
-            cityTextView.text = brewery.city + " - " + brewery.state
-            streetTextView.text = brewery.street
-            phoneTextView.text = brewery.phone
-            websiteTextView.text = brewery.websiteUrl
+            lat = brewery.latitude.toString()
+            lon = brewery.longitude.toString()
+            breweryName = brewery.name.toString()
+
+            nameTextView.text = breweryName
+            cityTextView.text = getString(R.string.city, brewery.city + " - " + brewery.state)
+            streetTextView.text = getString(R.string.street, brewery.street ?: "")
+            phoneTextView.text = getString(R.string.phone, brewery.phone ?: "")
+            websiteTextView.text = getString(R.string.website, brewery.websiteUrl ?: "")
+            ubicationTextView.text = getString(R.string.ubication, "")
         }
+
+        if (lat != "null") {
+            val smf: SupportMapFragment =
+                childFragmentManager.findFragmentById(R.id.ubicat) as SupportMapFragment
+            smf.getMapAsync(callback)
+        } else {
+            binding.ubicationTextView.text = getString(R.string.ubication, "unknown")
+        }
+
         return root
     }
-
 }
